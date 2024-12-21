32e658fefc807fa2ca288ac40718be3ad6fad1a4
BART! <bart@bartweb.cz>
2024-12-21 09:07:23 +0100

    feat(content): fixing parser

--- src/content.tsx
+++ src/content.tsx
@@ -1,37 +1,37 @@
-import { render } from "solid-js/web";
-import { insertAfter } from "./utils";
-import { CSFDMovie } from "node-csfd-api/types/interfaces/movie.interface";
+import { render } from 'solid-js/web';
+import { insertAfter } from './utils';
+import { CSFDMovie } from 'node-csfd-api/interfaces/movie.interface';
 import {
   calculateSeries,
   getButtonRootElements,
   getTitleAndYear,
-} from "./netflix/utils";
-import Csfd from "./components/Csfd";
+} from './netflix/utils';
+import Csfd from './components/Csfd';
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   // Listen for messages sent from background.js
-  if (request.message === "sendingUrl") {
-    if (request.url.includes("jbv=") || request.url.includes("/title/")) {
+  if (request.message === 'sendingUrl') {
+    if (request.url.includes('jbv=') || request.url.includes('/title/')) {
       const { title, year } = getTitleAndYear();
       if (title && year) {
         const isSeries = calculateSeries();
 
         console.log(
-          "CSFD – searching for",
+          'CSFD – searching for',
           `https://www.csfd.cz/hledat/?q=${encodeURIComponent(
-            title + " " + year
+            title + ' ' + year
           )}`,
           isSeries
         );
 
         sendResponse({ title, year, isSeries });
       } else {
-        console.error("CSFD – title or year not found", title, year);
+        console.error('CSFD – title or year not found', title, year);
       }
     }
   }
 
-  if (request.message === "movieFound") {
+  if (request.message === 'movieFound') {
     renderTemplate(request.movie);
   }
 });
@@ -40,7 +40,7 @@ const renderTemplate = (movie: CSFDMovie | null): void => {
   const { root, placingNode } = getButtonRootElements();
   if (root && placingNode) {
     insertAfter(placingNode, root);
-    console.warn("CSFD rendering:", movie);
-    render(() => (movie?.id ? <Csfd {...movie} /> : "Not found..."), root);
+    console.warn('CSFD rendering:', movie);
+    render(() => (movie?.id ? <Csfd {...movie} /> : 'Not found...'), root);
   }
 };
